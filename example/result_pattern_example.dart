c40e3cf80f74c4878232d3e8c97ea4bd4fc573ad
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2024-07-21 15:06:14 -0300

    Sample code

--- example/result_pattern_example.dart
+++ example/result_pattern_example.dart
@@ -1,6 +1,13 @@
 import 'dart:math';
 
+import 'package:result_pattern/async.dart';
 import 'package:result_pattern/result.dart';
+
+class Person {
+  final String name;
+  final int age;
+  const Person(this.name, this.age);
+}
 
 Result<String> taskThatCanFail() {
   if (Random().nextInt(6) == 0) {
@@ -9,10 +16,55 @@ Result<String> taskThatCanFail() {
   return Result.success('It is success');
 }
 
+FutureResult<List<Person>> getAllPeople() async {
+  // simulate some repository access
+  return const Result.success([
+    Person('Adonis', 30),
+    Person('Simon', 25),
+    Person('Pistis', 20),
+    Person('Selene', 15),
+    Person('Polyphemos', 10),
+  ]);
+}
+
+FutureResult<List<Person>> getPeopleAboveAge(int age) => getAllPeople().next(
+      (people) => Result.success(
+        people.where((person) => person.age > age).toList(),
+      ),
+    );
+
+FutureResult<List<Person>> getPeopleBellowAge(int age) async {
+  final people = await getAllPeople();
+  return switch (people) {
+    ResultSuccess<List<Person>>() => Result.success(
+        people.value.where((person) => person.age < age).toList(),
+      ),
+    ResultFailure<List<Person>>() => people,
+  };
+}
+
 void main() {
   taskThatCanFail().then((str) => Result.success(str.length)).fold((value) {
     print('Success: $value');
   }, onError: (error) {
     print('Error: $error');
+  });
+
+  getPeopleAboveAge(20).fold((people) {
+    print('People above 20:');
+    for (final person in people) {
+      print('  ${person.name}');
+    }
+  }, onError: (error) {
+    print('Some error happen: $error');
+  });
+
+  getPeopleBellowAge(20).fold((people) {
+    print('People bellow 20:');
+    for (final person in people) {
+      print('  ${person.name}');
+    }
+  }, onError: (error) {
+    print('Some error happen: $error');
   });
 }
